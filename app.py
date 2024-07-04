64d3afd425531c650998b052c354cad867faeb9c
Raghavan <raghavan.mit@gmail.com>
2024-07-03 21:49:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,9 @@ import json
 def get_commentary_gpt4o(input_json):
     prompt = json.dumps(input_json) + "Above is cricket commentary for one over. Understand the json above and generate a textual commentary for each ball with witty comment based one of the following personalities and their styles chosen at random from the list below list = [\"Chuck Norris\", \"Harsha Bhogle\", \"Rameez Raja\", \"Ravi Shastri\", \"Tony Greig\", \"geoffrey boycott\"] Commentary should include over, who bowled to who, what shot, runs scored and a witty comment based on the personality in bold, personality should also say what could have been different in italics. Put personality in brackets for debugging purpose"
     prompt += " return the commentary in a json list of string objects"
+    schema_to_follow = " Schema: \
+                        class response: \
+                            ball: String[]"
 
     response = client.chat.completions.create(
         model = "gpt-4o",
