951ca876521efb1bc94b9bbe7ba116f795b73126
Raghavan <raghavan.mit@gmail.com>
2024-07-03 22:34:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,10 @@ def get_ball_by_ball_commentary(URL):
 import json
 def get_commentary_gpt4o(input_json):
     
-    prompt = json.dumps(input_json) + "Above is cricket commentary for one over. Understand the json above and generate a textual commentary for each ball with witty comment based one of the following personalities and their styles chosen at random from the list below list = [\"Chuck Norris\", \"Harsha Bhogle\", \"Rameez Raja\", \"Ravi Shastri\", \"Tony Greig\", \"geoffrey boycott\", \"Richie Benaud\", \"David letterman\", \"Michael Holding\"] Commentary should include over, who bowled to who, what shot, runs scored and a witty comment based on the personality in bold, personality should also say what could have been different in italics. Put personality in brackets for debugging purpose. Use emojis and different colors, bold the witty comment"
+    prompt = json.dumps(input_json) + "Above is cricket commentary for one over. Understand the json above and generate a textual commentary for each ball with witty comment based one of the following personalities and their styles chosen at random from the list below list = [\"Chuck Norris\", \"Harsha Bhogle\", \"Rameez Raja\", \"Ravi Shastri\", \"Tony Greig\", \"geoffrey boycott\", \"Richie Benaud\", \"David letterman\", \"Michael Holding\"]"
+    prompt +=  "Commentary should include <b> over </b> :,  <b> ball number </b>,  who bowled to whom, what shot, what length and line, how many runs scored. "
+    prompt += " In the next line, <random color> <b> a witty comment based on the personality </b>, The personality should also say what could have been different in italics. Put personality in brackets for debugging purpose. Use emojis and different colors, bold the witty comment"
+    prompt += " Mix sarcasm, fun and make it engaging"
     prompt += " return the commentary as a json list of string objects one for each ball. It should STRICTLY be just JSON LIST of String objects with NO KEY"
    
 
